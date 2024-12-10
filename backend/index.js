69738872bdd3eb6fe7b16b7c311009d36f45905c
Abhinav.K <abhinavkkunnummal@gmail.com>
2024-12-10 18:36:06 +0530

    mongodb connected successfully

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,15 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
 
+mongoose.connect(process.env.Mongo).then(()=>{
+    console.log('Connected to MongoDB')
+}).catch((err)=>{
+    console.log(err)
+})
+
+mongoose.connect
 const app=express()
 
 app.listen(3000,()=>{
